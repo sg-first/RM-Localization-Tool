d3fc93852c8e80483c14fd08bcbe3ddd772be47e
3A <524019351@qq.com>
2023-04-01 17:06:02 +0800

    头像框

--- xlsToJson.py
+++ xlsToJson.py
@@ -3,16 +3,22 @@ import pandas as pd
 df = pd.read_excel('Localization.xls', 'Localization')
 wrapNum = 44
 
-start = 792
-end = 929
+start = 0
+end = 932
 col = 'cn'
 
 def addQuotes(s):
     return '\"'+str(s)+'\"'
 
+def changeNameStyle(s:str):
+    if s.find('：\\n') != -1:
+        s = '\\\\n<' + s
+        s = s.replace('：\\n', '>')
+    return s
+
 def autoWrap(s:str):
     s = s.replace('\\C', '\\\\C')
-    lines = s.split('\\n')
+    lines = s.split('\\n\\!')
     for i in range(len(lines)):
         line = lines[i]
         finishedSub = wrapNum
@@ -23,11 +29,12 @@ def autoWrap(s:str):
 
     s = lines[0]
     for i in range(1, len(lines)):
-        s += '\\n' + lines[i]
+        s += '\\\\n\\\\!' + lines[i]
     return s
 
 for index, row in df.iterrows():
     if index >= start and index <= end:
-        if row[col] != '':
+        if row[col] != '' and (not pd.isnull(row[col])) and (not pd.isna(row[col])):
+            row[col] = changeNameStyle(row[col])
             row[col] = autoWrap(row[col])
             print(addQuotes(row['key']) + ':' + addQuotes(row[col]) + ',')
