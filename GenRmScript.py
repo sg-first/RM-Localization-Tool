d6f9719019428d47c9a29dff40a6b326b9c1d29a
3A <524019351@qq.com>
2023-04-14 21:00:18 +0800

    最终剧本

--- GenRmScript.py
+++ GenRmScript.py
@@ -17,8 +17,9 @@ def getDialogueTemplate(content):
 newCode = []
 for index, row in df.iterrows():
     if index >= start and index <= end:
-        newCode.append(getShowDialogueTemplate())
-        newCode.append(getDialogueTemplate(row['key']))
+        if row['key'] != '' and (not pd.isnull(row['key'])) and (not pd.isna(row['key'])):
+            newCode.append(getShowDialogueTemplate())
+            newCode.append(getDialogueTemplate(row['key']))
 
 oldCode = '{"id":2,"name":"测试块","note":"","pages":[{"conditions":{"actorId":1,"actorValid":false,"itemId":1,"itemValid":false,"selfSwitchCh":"A","selfSwitchValid":false,"switch1Id":1,"switch1Valid":false,"switch2Id":1,"switch2Valid":false,"variableId":1,"variableValid":false,"variableValue":0},"directionFix":false,"image":{"characterIndex":0,"characterName":"","direction":2,"pattern":0,"tileId":0},"list":[{"code":101,"indent":0,"parameters":["",0,0,2]},{"code":401,"indent":0,"parameters":["啊啊"]},{"code":101,"indent":0,"parameters":["",0,0,2]},{"code":401,"indent":0,"parameters":["哦哦"]},{"code":0,"indent":0,"parameters":[]}],"moveFrequency":3,"moveRoute":{"list":[{"code":0,"parameters":[]}],"repeat":true,"skippable":false,"wait":false},"moveSpeed":3,"moveType":0,"priorityType":0,"stepAnime":false,"through":false,"trigger":0,"walkAnime":true}],"x":2,"y":1}'
 import json
--- xlsToJson.py
+++ xlsToJson.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 df = pd.read_excel('Localization.xls', 'Localization')
-wrapNum = 44
+wrapNum = {'cn':44, 'en': 89}
 
 start = 0
-end = 932
+end = 951
 col = 'cn'
 
 def addQuotes(s):
@@ -14,27 +14,29 @@ def changeNameStyle(s:str):
     if s.find('：\\n') != -1:
         s = '\\\\n<' + s
         s = s.replace('：\\n', '>')
-    return s
+    return s, s.find('>')
 
-def autoWrap(s:str):
+def autoWrap(s:str, nameEndSub:int):
+    name = s[:nameEndSub+1]  # 把名字截下来
+    s = s[nameEndSub+1:]
     s = s.replace('\\C', '\\\\C')
     lines = s.split('\\n\\!')
     for i in range(len(lines)):
         line = lines[i]
-        finishedSub = wrapNum
+        finishedSub = wrapNum[col]
         while len(line) > finishedSub:
             line = line[:finishedSub] + '\\n' + line[finishedSub:]
-            finishedSub += 1+wrapNum
+            finishedSub += 1+wrapNum[col]
         lines[i] = line
 
     s = lines[0]
     for i in range(1, len(lines)):
-        s += '\\\\n\\\\!' + lines[i]
-    return s
+        s += '\\n\\\\!' + lines[i]
+    return name + s
 
 for index, row in df.iterrows():
     if index >= start and index <= end:
         if row[col] != '' and (not pd.isnull(row[col])) and (not pd.isna(row[col])):
-            row[col] = changeNameStyle(row[col])
-            row[col] = autoWrap(row[col])
+            row[col], nameEndSub = changeNameStyle(row[col])
+            row[col] = autoWrap(row[col], nameEndSub)
             print(addQuotes(row['key']) + ':' + addQuotes(row[col]) + ',')
